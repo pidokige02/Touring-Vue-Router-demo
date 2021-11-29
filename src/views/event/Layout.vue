20a8d22b1fe91b1a5f4bc3dc0d34132b73af6339
pidokige02 <pidokige0204@gmail.com>
2021-11-29 22:31:42 +0900

    updated for nested routers

---
+++ src/views/event/Layout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div v-if="event">
+    <h1>{{ event.title }}</h1>
+    <div id="nav">
+      <!-- Since :id is reuired for each child path:
+        if :id isn't sent in, it will look and use the :id param that is present.
+      -->
+      <router-link :to="{ name: 'EventDetails' }">Details</router-link>
+      |
+      <router-link :to="{ name: 'EventRegister' }">Register</router-link>
+      |
+      <router-link :to="{ name: 'EventEdit' }">Edit</router-link>
+    </div>
+    <router-view :event="event" />
+  </div>
+</template>
+
+
+<script>
+import EventService from '@/services/EventService.js'
+export default {
+  props: ['id'],
+  data() {
+    return {
+      event: null
+    }
+  },
+  created() {
+    EventService.getEvent(this.id) //use the 'id' to get the event information from the API.
+      .then(response => {
+        this.event = response.data
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }
+}
+</script>
